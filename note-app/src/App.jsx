e1053da6f58ea0dfb4321d1c8e64da450f05cefb
mansi kasundra <mansikasundra33@gmail.com>
2025-08-23 13:25:06 +0530

    [mk][project set up]

---
+++ note-app/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1> hello</h1>
+      <header>
+        <nav>
+          <li>
+            <ul>home</ul>
+          </li>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default App;
